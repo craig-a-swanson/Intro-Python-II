5726db2df216acf205867cc749db25fca4e892ce
Craig Swanson <cswanson02@me.com>
2020-09-09 19:50:14 -0500

    Added two subclasses to Item

--- src/item.py
+++ src/item.py
@@ -4,3 +4,39 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+    
+    def __str__(self):
+        return f'{self.name}: {self.description}\n'
+
+
+class Light(Item):
+    def __init__(self, name, description, luminosity, longevity):
+        super().__init__(name, description)
+        self.luminosity = luminosity
+        self.longevity = longevity
+    
+    def __str__(self):
+        base_string = super().__str__()
+        return f'{base_string}\tLuminosity: {self.luminosity}\n\tLongevity: {self.longevity}\n'
+
+class Reference(Item):
+    def __init__(self, name, description, size):
+        super().__init__(name, description)
+        self.size = size
+    
+    def __str__(self):
+        base_string = super().__str__()
+        return f'{base_string}\tSize: {self.size}'
+
+
+
+
+
+# Code used for testing
+# torch = Light("Torch", "Oil soaked rags on a stick", 3, 2)
+# flashlight = Light("Flashlight", "Handheld metal cylinder with an LED bulb", 5, 8)
+# spellbook = Reference("Book of Spells", "A large reference manual containing all sorts of chants and stuff", 10)
+
+# print(torch)
+# print(flashlight)
+# print(spellbook)
